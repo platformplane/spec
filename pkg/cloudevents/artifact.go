6ff88f08d67b7b63bea674633b91286b797dc959
Fabian Gehri <fabian.gehri@zuehlke.com>
2025-02-12 11:10:22 +0100

    add artifact queuing failed event

--- pkg/cloudevents/artifact.go
+++ pkg/cloudevents/artifact.go
@@ -1,8 +1,19 @@
 package cloudevents
 
 const (
-	ArtifactSyncFailedType = "io.platformplane.artifact.sync.failed"
+	ArtifactQueuingFailedType = "io.platformplane.artifact.queuing.failed"
+	ArtifactSyncFailedType    = "io.platformplane.artifact.sync.failed"
 )
+
+type ArtifactQueuingFailed struct {
+	Purl string `json:"purl"`
+
+	ErrorMessage string `json:"error_message"`
+
+	LastAttempt bool `json:"last_attempt"`
+
+	CorrelationID *string `json:"correlation_id"`
+}
 
 type ArtifactSyncFailed struct {
 	Purl string `json:"purl"`
